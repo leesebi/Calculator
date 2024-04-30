2279a75a845860c0b11baaf9cde1604dea3b4e2d
이세비 <lsy205105@naver.com>
2024-04-30 20:52:12 +0900

    level 2-1

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,28 +1,47 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Calculator {
     private List<Integer> res = new ArrayList<>();
+
+    public int add(int x, int y) {
+        return x+y;
+    }
+
+    public int sub(int x, int y){
+        return x-y;
+    }
+
+    public int multi(int x, int y){
+        return x * y;
+    }
+
+    public int divide(int x, int y){
+        return x/y;
+    }
+
+
     public int calculate(int num1, int num2, char op) throws InputExcetpion {
         int result = 0;
 
         switch (op){
             case '+' -> {
-                result = num1 + num2;
+                result = add(num1, num2);
             }
             case '-' -> {
-                result = num1 - num2;
+                result = sub(num1, num2);
             }
             case '*' -> {
-                result = num1 * num2;
+                result = multi(num1, num2);
             }
             case '/' -> {
                 if(num2 <= 0) {
                     throw new InputExcetpion();
                 }
-
-                result = num1 / num2;
+                result = divide(num1, num2);
             }
+
             default -> throw new InputExcetpion();
         }
         res.add(result);
