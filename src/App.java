8677b08cf663fb8cd6a87c176408659b17ba17dc
이세비 <lsy205105@naver.com>
2024-04-30 20:41:53 +0900

    level 2-2

--- src/App.java
+++ src/App.java
@@ -4,58 +4,27 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InputExcetpion{
         Scanner sc = new Scanner(System.in);
-        List<Integer> ans = new ArrayList<>();
 
         boolean flag = true;
-        while (flag){
-            System.out.print("첫 번째 숫자를 입력하세요 :");
+        Calculator calculator;
+        while(flag){
+            calculator = new Calculator();
+            System.out.print("첫 번째 숫자를 입력하세요:");
             int num1 = sc.nextInt();
-
-            System.out.print("두 번째 숫자를 입력하세요 :");
+            System.out.print("두 번째 숫자를 입력하세요:");
             int num2 = sc.nextInt();
 
-            System.out.print("사칙연산 기호를 입력하세요 :");
-            char op = sc.next().charAt(0);
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char operator = sc.next().charAt(0);
 
-            int result = 0;
-            switch (op){
-                case '+' -> {
-                    result = num1 + num2;
-                }
-                case '-' -> {
-                    result = num1 - num2;
-                }
-                case '*' -> {
-                    result = num1 * num2;
-                }
-                case '/' -> {
-                    if(num2 == 0) {
-                        System.out.println("나눗셈 연산에서 분모에 0이 입력될 수 없습니다.");
-                        break;
-                    }
-                    result = num1 / num2;
-                }
-            }
-            ans.add(result);
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            String st = sc.next();
-
-            if(st.equals("remove")){
-                ans.remove(0);
-            }
-
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            String in = sc.next();
-            if(in.equals("inquiry")){
-                for(int x:ans) System.out.print(x+" ");
-            }
+            System.out.println(calculator.calculate(num1,num2, operator));
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            String exit = sc.next();
-            if(exit.equals("exit")) flag = false;
-        }
+            String st = sc.next();
 
+            if(st.equals("exit")) flag = false;
+        }
     }
 }
